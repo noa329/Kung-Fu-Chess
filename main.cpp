da21006f68d47e33d13dd3b14833bd2295028115
Noa Meidan <noa9866@gmail.com>
2026-07-09 00:32:28 +0300

    Fix: update Game header file extension from .h to .hpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include "Game.h"
+#include "Game.hpp"
 
 static inline std::string trim(const std::string& s) {
     size_t start = s.find_first_not_of(" \t\r\n");
